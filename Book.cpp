c8498044b572bc79d28d639dc88e140bf1bb5c41
harisdesai <harisdesai007@gmail.com>
2025-02-10 21:27:53 -0800

    Update Book.cpp

--- Book.cpp
+++ Book.cpp
@@ -11,6 +11,7 @@ class Book{
 	   int chapter_number;
 	public:
 		Book(){
+		    cout<<"Default constructor"<<endl;
 			Title=new char[1];
 			Title[0]='\0';
 			Author=new char[1];
@@ -22,6 +23,7 @@ class Book{
 		}
 		Book(const char* t, const char* a, int pages, bool format, float* chap_pg,int chap_num)
 		{
+		    cout<<"Parameterised constructor"<<endl;
 			Title=new char[strlen(t)+1];
 			strcpy(Title,t);
 			
@@ -32,11 +34,14 @@ class Book{
 			format_type=format;
 			chapter_pg= new float[chap_num];
 			for (int i = 0; i < chap_num; i++) {
-            chapter_pg[i] = page_count / float(chap_num);
+            chapter_pg[i] =chap_pg[i];
+            
         }
 			chapter_number=chap_num;
 		}
+		
 		Book(const Book &obj){
+		    cout<<"Copy constructor"<<endl;
 			Title=obj.Title;
 			Author=obj.Author;
 			page_count=obj.page_count;
@@ -44,30 +49,45 @@ class Book{
 			chapter_pg=obj.chapter_pg;
 			chapter_number=obj.chapter_number;
 		}
+		
 		void setChapterpagecount(int x){
 			page_count=x;
 		}
+		
 		void getinfo(){
+		    cout<<endl;
 			cout<<"Book Name :"<<Title << endl; 
 			cout<<"Author Name :"<<Author <<endl;
 			cout<<"Page Count :"<<page_count<<endl;
-			cout<<"Format type : ";
+			cout<<"Format Type : ";
 			if(format_type == 1){
 				cout<<"Hardcover" <<endl;
 			}
 			else{
 				cout<<"Softcover"<<endl;
 			}
-			cout<<"Chapter Pages :";
-			for(int i=0;i<chapter_number;i++)
-				cout<<chapter_pg[i];
-			cout<<endl;
 			cout<<"number of chapters :" <<chapter_number<<endl;
+			cout<<"Chapter Pages :";
+			for(int i=0;i<chapter_number;i++){
+				cout<<chapter_pg[i];
+			    cout<<"  ";
+			}
+			cout<<endl<<endl;
+		}
+		
+		~Book(){
+		    delete Title;
+		    delete Author;
+		    delete chapter_pg;
 		}
 };
 int main() {
 	Book b1;
+	b1.getinfo();
 	float arr[20]={10,11,12,13,14,15,16,17,18,19};
-	Book b2("BOOK","Haris",121,true,arr,10);
+	Book b3("BOOK","Haris",121,true,arr,10);
+	b3.getinfo();
+	Book b2=b3;
+	b2.setChapterpagecount(500);
 	b2.getinfo();
 }
